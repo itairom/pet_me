88b2c363536708f0841b50a45a4c53fff3fe0760
itairom <iitaiirom@gmail.com>
2021-05-27 16:03:23 +0300

    sds

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,11 +4,15 @@ import { petService } from '../services/petService.js'
 export function Home() {
 
      const pets= petService.query()
+    //  const pet = petService.getpetByid()
+    //  console.log(pet);
      console.log(pets);
     return (
         <section className="home">
             <h1>Home</h1>
             <h2>{pets[0].name}</h2>
+
+            
 
         </section>
     )
