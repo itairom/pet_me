e72c0d3a0ec82bbbab755aff4d6a7cf85f85b492
eyal258@gmail.com <eyal258@gmail.com>
2021-05-27 13:52:16 +0300

    new data

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,9 @@ export function Home(){
     return (
         <section className="home">
             <h1>Home</h1>
+        {/* list  */}
+            <List/>
+            <List/>
         </section>
     )
 }
