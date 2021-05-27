7c4e06d1102cfc02de0b2cbc20d599e783bb532f
itairom <iitaiirom@gmail.com>
2021-05-27 13:53:20 +0300

    sds

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 
+
 import { Home } from './pages/Home'
 
 
---
+++ frontend/src/store/reducers/petReducer.js
@@ -0,0 +1,20 @@
+const initialState = {
+    pets: [],
+    pet: null
+}
+
+
+export function petReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'SET_PETS':
+            return { ...state, pets: action.pets }
+        case 'ADD_PET':
+            return { ...state, pets: [...state.pets, action.pet] }
+        case 'REMOVE_PET':
+            console.log(action.petId);
+            console.log(state.pets);
+            return { ...state, pets: state.pets.filter(pet => pet._id !== action.petId) }
+        default:
+            return state
+    }
+}
