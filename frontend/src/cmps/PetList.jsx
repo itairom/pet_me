9645b902708224e9fe81c677467ed52290a8d519
gute1993-idan <igutman93@gmail.com>
2021-05-28 10:41:38 +0300

    ggg

--- frontend/src/cmps/PetList.jsx
+++ frontend/src/cmps/PetList.jsx
@@ -1,10 +1,6 @@
 import { PetPreview } from '../cmps/PetPreview.jsx'
 
 export function PetList({pets}) {
-<<<<<<< HEAD
-    console.log(pets);
-=======
->>>>>>> f30257cb64b8ef4a04a72c6b046795bad32e3a0f
     return (
         <section className="card-list">
             {pets.map(pet => <PetPreview key={pet._id}
