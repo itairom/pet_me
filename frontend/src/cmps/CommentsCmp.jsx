3bee0a8099f410acc0015fe78dcdd60c0489f222
gute1993-idan <igutman93@gmail.com>
2021-06-02 10:07:28 +0300

    10 idan

--- frontend/src/cmps/CommentsCmp.jsx
+++ frontend/src/cmps/CommentsCmp.jsx
@@ -11,26 +11,26 @@ class _CommentsCmp extends Component {
         return (
             <section className="comments-container" >
                 <div className="comments-btns flex">
-                    <button className="add-comment-btn" onClick={ () => addComment(pet._id) }>Add comment</button>
+                    <button className="add-comment-btn" onClick={() => addComment(pet._id)}>Add comment</button>
                     <button className="opan-chat-btn">Chat with the Owner</button>
                 </div>
                 <h3 className="comments-head">Comments</h3>
-                <ul>
-                    { pet.comments.map(comment =>
-                        <div key={ comment.id } className="comments-list">
+                <ul className="clean-list">
+                    {pet.comments.map(comment =>
+                        <div key={comment.id} className="comments-list">
                             <li>
                                 <div className="reviewer-preview-card flex column">
-                                    <img src={ comment.by.imgUrl } alt="skeleton" />
+                                    <img src={comment.by.imgUrl} alt="skeleton" />
                                     <div className="flex column">
-                                        <span className="commenter-name">{ comment.by.fullname }</span>
-                                        {/* TODO: reconfigure date by fixed times */ }
-                                        <span className="comment-time">{ utilService.timeSince(comment.created, 'ago') }</span>
+                                        <span className="commenter-name">{comment.by.fullname}</span>
+                                        {/* TODO: reconfigure date by fixed times */}
+                                        <span className="comment-time">{utilService.timeSince(comment.created, 'ago')}</span>
                                     </div>
-                                    <LongTxt className="comment-desc" txt={ comment.txt } />
+                                    <LongTxt className="comment-desc" txt={comment.txt} />
                                 </div>
                             </li>
                         </div>
-                    ) }
+                    )}
                 </ul>
                 <div className="new-comment-modal">
 
